4931efb4d481afad0945a710f10834797790c9b4
Nick Foden <nickfoden@gmail.com>
2017-06-14 15:28:13 -0400

    Changing boards

--- src/components/board.js
+++ src/components/board.js
@@ -4,15 +4,36 @@ import List from './list';
 
 import './board.css';
 
-export default function Board() {
-    return (
-        <div className="board">
-            <h2>Example board</h2>
-            <div className="lists">
-                <List />
-                <List />
-                <List />
+export default class Board extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lists: [{
+                title: 'Example list 1'
+            }, {
+                title: 'Example list 2'
+            }]
+        };
+    }
+
+    render() {
+        const lists = this.state.lists.map((list, index) =>
+            <List key={index} {...list} />
+        );
+
+        return (
+            <div className="board">
+                <h2>{this.props.title}</h2>
+                <div className="lists">
+                    {lists}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+Board.defaultProps = {
+    title: 'Board'
+};
+
